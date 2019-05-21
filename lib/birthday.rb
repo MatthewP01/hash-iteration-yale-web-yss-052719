5aee98f16ac4248ad5cd41a64e8f5cc1dd736f0c
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 19:56:35 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,12 +16,6 @@ end
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids = {
-    "Timmy" => 9,
-    "Sarah" => 6,
-    "Amanda" => 27
-  }
-
   age_appropriate_birthday.each do |key, value|
     if value <= 12
       puts "Happy Birthday #{key}! You are now #{value} years old!"
