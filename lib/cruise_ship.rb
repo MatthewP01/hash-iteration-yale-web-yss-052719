4b1e454caa9a57aa97ecb7c0ba8f177920f28970
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-21 20:05:25 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,6 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   # add the code snippet here!
-  winner = passengers[suite_a:].join(" ")
-  return winner
+  winner = passengers[suite_a:]
+  return winner.join(" ")
 end
